bc299b8a46d604f89133523e8e04e7968693daed
gretmn102 <erlgreywatson@gmail.com>
2021-11-18 14:45:19 +0200

    remove NavMenu

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import { ApplicationState } from './store'
 import Home from './components/Home'
 import SignIn from './components/SignIn'
 import SignUp from './components/SignUp'
-import NavMenu from './components/NavMenu'
 import * as SessionSlice from './store/sessionSlice'
 
 import './custom.css'
@@ -20,17 +19,12 @@ export default function App() {
 
   if (sessionState.isLogin) {
     return (
-      <>
-        <NavMenu />
-        <Container>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/login' component={SignIn} />
-            <Route path='/sign-up'><SignUp /></Route>
-            <Route component={Home} />
-          </Switch>
-        </Container>
-      </>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/login' component={SignIn} />
+        <Route path='/sign-up'><SignUp /></Route>
+        <Route component={Home} />
+      </Switch>
     )
   }
   return (
